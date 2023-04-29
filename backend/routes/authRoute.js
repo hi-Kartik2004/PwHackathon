0dc8b1393f718c401bed61fc41a3d1e55b7771b8
abhirajn <abhin.tmk@gmail.com>
2023-04-30 00:42:30 +0530

    updated backend

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { register } from "../authControl.js";
+import { Router } from "express";
+
+const router = express.Router();
+
+router.post("/register", register)
+// router.post("/login", login)
+// router.post("/logout", logout)
+
+export default router;
