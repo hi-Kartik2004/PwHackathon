1f7e25574b589c5ad45f233f2639a12293aba735
abhirajn <abhin.tmk@gmail.com>
2023-04-30 00:59:35 +0530

    updated loginPage

--- backend/authControl.js
+++ backend/authControl.js
@@ -9,11 +9,11 @@ export const register = async (req, res, next) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
-    //   ...req.body,
-    //   password: hash,
-      username : req.body.username,
-      email : req.body.email,
-      password : req.body.password
+      ...req.body,
+      password: hash,
+    //   username : req.body.username,
+    //   email : req.body.email,
+    //   password : req.body.password
 
     });
 
@@ -23,42 +23,42 @@ export const register = async (req, res, next) => {
     next(err);
   }
 };
-// export const login = async (req, res, next) => {
-//   try {
-//     const user = await User.findOne({ username: req.body.username });
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
 
-//     if (!user) return next(createError(404, "User not found!"));
+    if (!user) return ( "User not found!");
 
-//     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-//     if (!isCorrect)
-//       return next(createError(400, "Wrong password or username!"));
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return ( "User not found!");
 
-//     const token = jwt.sign(
-//       {
-//         id: user._id,
-//         isSeller: user.isSeller,
-//       },
-//       process.env.JWT_KEY
-//     );
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
 
-//     const { password, ...info } = user._doc;
-//     res
-//       .cookie("accessToken", token, {
-//         httpOnly: true,
-//       })
-//       .status(200)
-//       .send(info);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
+  } catch (err) {
+    next(err);
+  }
+};
 
-// export const logout = async (req, res) => {
-//   res
-//     .clearCookie("accessToken", {
-//       sameSite: "none",
-//       secure: true,
-//     })
-//     .status(200)
-//     .send("User has been logged out.");
-// };
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
+};
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { register } from "../authControl.js";
+import { register , login } from "../authControl.js";
 import { Router } from "express";
 
 const router = express.Router();
 
 router.post("/register", register)
-// router.post("/login", login)
+router.post("/login", login)
 // router.post("/logout", logout)
 
 export default router;
